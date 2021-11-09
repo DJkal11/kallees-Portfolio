17b2f8d2ca1d88a3ffe13aabd0d0e7469bef5ddc
Lucian Kallee <luciankallee@gmail.com>
2021-11-09 16:17:34 +0200

    Created the landing page.

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,58 @@
 <script>
-	export let name;
 </script>
 
-<h1>Hello world</h1>
+<div class="main">
+  <div class="header">
+    <nav>
+      <ul>
+        <li href="">Logo</li>
+        <li href="portfolio">Portfolio</li>
+        <li href="about">About</li>
+      </ul>
+    </nav>
+    <div class="intro">
+      <h3 class="name">-- Lucian Kallee</h3>
+      <h1>Hello my name is Lucian and I'm a fullstack developer.</h1>
+    </div>
+  </div>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+	@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap');
+  .header {
+    font-family: 'Raleway', sans-serif;
+	color: #EEEEEE;
+  }
+
+  .main{
+	  height: 100vh;
+	  background-color: #222831;
+  }
+
+  .intro{
+	  padding: 90px;
+	  margin-top: 100px;
+	  margin-right:50%;
+  }
+
+  nav{
+	text-align: center;
+    padding: 1em;
+	margin: 0 auto;
+  }
+
+  ul {
+    list-style-type: none;
+	overflow: hidden;
+	display: flex;
+	flex-direction: row;
+  }
+   li{
+	   padding-left:30px ;
+	   cursor: pointer;
+   }
+
+   .name{
+	color: #00ADB5;
+   }
 </style>

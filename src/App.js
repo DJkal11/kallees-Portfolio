f89aeea7db9400f66816cbb0c4f186d4319020c4
Lucian Kallee <luciankallee@gmail.com>
2021-11-25 09:53:21 +0200

    Adding Welcome page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Welcome from './components/welcome';
+import Explore from './components/explore';
+import Summon from './components/summon';
+import Help from './components/help';
+import Community from './components/community-links';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [ border1, setBorder1 ] = useState(true);
+  const [ border2, setBorder2 ] = useState(false);
+  const [ border3, setBorder3 ] = useState(false);
+
+  function handleClickWelcome() {
+    setBorder1(true);
+    setBorder2(false);
+    setBorder3(false);
+  }
+
+  function handleClickCryptobank() {
+    setBorder1(false);
+    setBorder2(true);
+    setBorder3(false);
+  }
+
+  function handleClickExchange() {
+    setBorder1(false);
+    setBorder2(false);
+    setBorder3(true);
+  }
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+     <div className="grid grid-cols-2">
+        <nav className="h-screen w-60 border-r-2 border-gray-300">
+          <ul>
+            <li className="mb-20 p-7" ><b>Mochi</b></li>
+            <li onClick={handleClickWelcome} id="nav-item-1" className={border1 ? 'border-on font-bold text-gray-400 p-5 pl-7' : 'border-off font-bold text-gray-400 p-5 pl-7'}>
+              <Link to="/">Welcome</Link>
+            </li>
+            <li onClick={handleClickCryptobank} id="nav-item-2" className={border2 ? "border-on font-bold text-gray-400 p-5 pl-7" : "border-off font-bold text-gray-400 p-5 pl-7" }>
+              <Link to="/Explore">Explore</Link>
+            </li>
+            <li onClick={handleClickExchange} id="nav-item-3" className={border3 ? "border-on font-bold text-gray-400 p-5 pl-7" : "border-off font-bold text-gray-400 p-5 pl-7"}>
+              <Link to="/Summon">Exchange</Link>
+            </li>
+            <li onClick={handleClickExchange} id="nav-item-3" className={border3 ? "border-on font-bold text-gray-400 p-5 pl-7" : "border-off font-bold text-gray-400 p-5 pl-7"}>
+              <Link to="/Help">Exchange</Link>
+            </li>
+            <li onClick={handleClickExchange} id="nav-item-3" className={border3 ? "border-on font-bold text-gray-400 p-5 pl-7" : "border-off font-bold text-gray-400 p-5 pl-7"}>
+              <Link to="/Community">Exchange</Link>
+            </li>
+          </ul>
+        </nav>
+
+        
+        <Routes>
+          <Route path="/" element={<Welcome/>}/>
+          
+          <Route path="/Explore" element={<Explore/>}/>
+        
+          <Route path="/Summon" element={<Summon/>}/>
+
+          <Route path="/Help" element={<Help/>}/>
+
+          <Route path="/Community" element={<Community/>}/>
+           
+        </Routes>
+      </div>
+
+   </Router>
+    
   );
 }
 

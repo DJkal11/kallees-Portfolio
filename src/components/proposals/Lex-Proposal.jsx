a5448c4881d316fbfaf4d11aa64dba50249f614f
Lucian Kallee <luciankallee@gmail.com>
2021-11-29 20:35:32 +0200

    Updated transition and button

--- src/components/proposals/Lex-Proposal.jsx
+++ src/components/proposals/Lex-Proposal.jsx
@@ -4,7 +4,7 @@ import { IoArrowBack } from 'react-icons/io5';
 function LexProposal() {
   return (
     <div
-      className="transitionProposal max-w-screen-2xl h-screen height-mobile margin-mobile bg-red-400 margin text-white"
+      className="transitionProposal max-w-screen-2xl h-full height-mobile margin-mobile bg-red-400 margin text-white"
       style={{
         backgroundImage:
           "url(https://images.unsplash.com/photo-1629984164142-21c2039de926?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80)",
@@ -48,46 +48,46 @@ function LexProposal() {
       <div className="grid grid-cols-2 welcome pl-44 pr-36 proposal-padding-mobile proposal-padding-tablet">
         
 
-        <div className=" w-80 h-60 m-10 mb-7 mr-0 rounded-3xl colour text-white">
+        <div className=" w-80 h-auto m-10 mb-7 mr-0 rounded-3xl colour text-white">
           <div className="p-8">
             <div className="">
-              <h1 className="text-3xl">MEMBER PROPOSAL</h1>
-              <h1 className="text-2xl">Request to Join The FoundationsDAO</h1>
-              <h3 className="text-xl">Submitted Nov 18,2021</h3>
-              <h1 className="text-lg">Unsponsored</h1>
+              <h1 className="text-4xl">MEMBER PROPOSAL</h1>
+              <h1 className="text-3xl">Request to Join FoundationsDAO</h1>
+              <h3 className="text-2xl">Submitted Nov 18,2021</h3>
+              <h1 className="text-xl">Unsponsored</h1>
             </div>
           </div>
         </div>
 
-        <div className="w-80 h-60 m-10 mb-7 mr-0 rounded-3xl colour text-white">
+        <div className="w-80 h-auto m-10 mb-7 mr-0 rounded-3xl colour text-white">
           <div className="p-8">
             <div className="">
-              <h1 className="text-3xl">WHITELIST TOKEN PROPOSAL</h1>
-              <h2 className="text-2xl">Token</h2>
-              <h3 className="text-xl">Submitted Nov 18,2021</h3>
-              <h1 className="text-lg">Unsponsored</h1>
+              <h1 className="text-4xl">WHITELIST TOKEN PROPOSAL</h1>
+              <h2 className="text-3xl">Token</h2>
+              <h3 className="text-2xl">Submitted Nov 18,2021</h3>
+              <h1 className="text-xl">Unsponsored</h1>
             </div>
           </div>
         </div>
 
-        <div className="w-80 h-60 m-10 mb-7 mr-0 rounded-3xl colour text-white">
+        <div className="w-80 h-auto m-10 mb-7 mr-0 rounded-3xl colour text-white">
           <div className="p-8">
             <div className="">
-              <h1 className="text-3xl">MEMBER PROPOSAL</h1>
-              <h2 className="text-2xl">New Member: Justin Ho</h2>
-              <h3 className="text-xl">Submitted Nov 19,2021</h3>
-              <h1 className="text-lg">Unsponsored</h1>
+              <h1 className="text-4xl">MEMBER PROPOSAL</h1>
+              <h2 className="text-3xl">New Member: Justin Ho</h2>
+              <h3 className="text-2xl">Submitted Nov 19,2021</h3>
+              <h1 className="text-xl">Unsponsored</h1>
             </div>
           </div>
         </div>
 
-        <div className="w-80 h-60 m-10 mb-7 mr-0 rounded-3xl colour text-white">
+        <div className="w-80 h-auto m-10 mb-7 mr-0 rounded-3xl colour text-white">
           <div className="">
             <div className="p-8">
-              <h1 className="text-3xl">MEMBER PROPOSAL</h1>
-              <h2 className="text-2xl">Request Shares For Tokens</h2>
-              <h3 className="text-xl">Submitted Nov 20,2021</h3>
-              <h1 className="text-lg">Unsponsored</h1>
+              <h1 className="text-4xl">MEMBER PROPOSAL</h1>
+              <h2 className="text-3xl">Request Shares For Tokens</h2>
+              <h3 className="text-2xl">Submitted Nov 20,2021</h3>
+              <h1 className="text-xl">Unsponsored</h1>
             </div>
           </div>
         </div>
--- src/components/proposals/Raid-Proposal.jsx
+++ src/components/proposals/Raid-Proposal.jsx
@@ -5,7 +5,7 @@ import { IoArrowBack } from 'react-icons/io5';
 function RaidProposal() {
   return (
     <div
-      className="transitionProposal max-w-screen-2xl h-screen height-mobile margin-mobile bg-red-400 margin text-white"
+      className="transitionProposal max-w-screen-2xl h-full height-mobile margin-mobile bg-red-400 margin text-white"
       style={{
         backgroundImage:
           "url(https://images.unsplash.com/photo-1629984164142-21c2039de926?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80)",
@@ -48,46 +48,46 @@ function RaidProposal() {
       <div className="grid grid-cols-2 welcome pl-44 pr-36 proposal-padding-mobile proposal-padding-tablet">
         
 
-        <div className="shadow-2xl w-80 h-60 m-10 mb-7 mr-0 rounded-3xl colour text-white">
+        <div className="shadow-2xl w-80 h-auto m-10 mb-7 mr-0 rounded-3xl colour text-white">
           <div className="p-8">
             <div className="">
-              <h1 className="text-3xl">FUNDING PROPOSAL</h1>
-              <h1 className="text-2xl">submitted Member Proposal</h1>
-              <h3 className="text-xl">Submitted Nov 13,2021</h3>
-              <h1 className="text-lg">Unsponsored</h1>
+              <h1 className="text-4xl">FUNDING PROPOSAL</h1>
+              <h1 className="text-3xl">submitted Member Proposal</h1>
+              <h3 className="text-2xl">Submitted Nov 13,2021</h3>
+              <h1 className="text-xl">Unsponsored</h1>
             </div>
           </div>
         </div>
 
-        <div className="w-80 h-60 m-10 mb-7 mr-0 rounded-3xl colour text-white">
+        <div className="w-80 h-auto m-10 mb-7 mr-0 rounded-3xl colour text-white">
           <div className="p-8">
             <div className="">
-              <h1 className="text-3xl">FUNDING PROPOSAL</h1>
-              <h1 className="text-2xl">Want to join the membership</h1>
-              <h3 className="text-xl">Submitted Nov 13,2021</h3>
-              <h1 className="text-lg">Unsponsored</h1>
+              <h1 className="text-4xl">FUNDING PROPOSAL</h1>
+              <h1 className="text-3xl">Want to join the membership</h1>
+              <h3 className="text-2xl">Submitted Nov 13,2021</h3>
+              <h1 className="text-xl">Unsponsored</h1>
             </div>
           </div>
         </div>
 
-        <div className="w-80 h-60 m-10 mb-7 mr-0 rounded-3xl colour text-white">
+        <div className="w-80 h-auto m-10 mb-7 mr-0 rounded-3xl colour text-white">
           <div className="p-8">
             <div className="">
-              <h1 className="text-3xl">FUNDING PROPOSAL</h1>
-              <h2 className="text-2xl">We need to triple our contribution</h2>
-              <h3 className="text-xl">Submitted Nov 13,2021</h3>
-              <h1 className="text-lg">Unsponsored</h1>
+              <h1 className="text-4xl">FUNDING PROPOSAL</h1>
+              <h2 className="text-3xl">We need to triple our contribution</h2>
+              <h3 className="text-2xl">Submitted Nov 13,2021</h3>
+              <h1 className="text-xl">Unsponsored</h1>
             </div>
           </div>
         </div>
 
-        <div className="w-80 h-60 m-10 mb-7 mr-0 rounded-3xl colour text-white">
+        <div className="w-80 h-auto m-10 mb-7 mr-0 rounded-3xl colour text-white">
           <div className="p-8">
             <div className="">
-              <h1 className="text-3xl">MEMBER PROPOSAL</h1>
-              <h2 className="text-2xl">Request to Join The FoundationsDAO</h2>
-              <h3 className="text-xl">Submitted Nov 13,2021</h3>
-              <h1 className="text-lg">Unsponsored</h1>
+              <h1 className="text-4xl">MEMBER PROPOSAL</h1>
+              <h2 className="text-3xl">Request to Join FoundationsDAO</h2>
+              <h3 className="text-2xl">Submitted Nov 13,2021</h3>
+              <h1 className="text-xl">Unsponsored</h1>
             </div>
           </div>
         </div>
